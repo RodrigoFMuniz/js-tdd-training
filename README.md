c6857145a52fc7622a81a2204051bfe74d52c479
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-02-12 20:55:59 -0300

    Correct sintaxe of It. Missed callback function

--- README.md
+++ README.md
@@ -38,20 +38,21 @@
 
       describe('method A', function(){
         context('Case 1', function(){
-          it('Should happen something')
+          it('Should happen something', function(){
           // Espera que algo aconteça
           // Entrada de dados / método soma(2,2)
           // Espera retornar / (4) => true | (3) => (false) broken test
+          })
         })
 
         context('Case 2', function(){
-          it('Should happen something else)
+          it('Should happen something else', function(){})
         })
       })
 
       describe('method B', function(){
         context('Case 1', function(){
-          it('Should happen something to case 1 of method B')
+          it('Should happen something to case 1 of method B', function(){})
         })
       })
     })
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,22 +1,23 @@
-describe('nome da classe',function(){
+describe('nome da classe', function () {
   //Aqui roda-se os testes.
 
-  describe('method A', function(){
-    context('Case 1', function(){
-      it('Should happen something')
-      // Espera que algo aconteça
-      // Entrada de dados / método soma(2,2)
-      // Espera retornar / (4) => true | (3) => (false) broken test
+  describe('method A', function () {
+    context('Case 1', function () {
+      it('Should happen something', function () {
+        // Espera que algo aconteça
+        // Entrada de dados / método soma(2,2)
+        // Espera retornar / (4) => true | (3) => (false) broken test
+      })
     })
 
-    context('Case 2', function(){
-      it('Should happen something else')
+    context('Case 2', function () {
+      it('Should happen something else', function () { })
     })
   })
 
-  describe('method B', function(){
-    context('Case 1', function(){
-      it('Should happen something to case 1 of method B')
+  describe('method B', function () {
+    context('Case 1', function () {
+      it('Should happen something to case 1 of method B', function () { })
     })
   })
 })
