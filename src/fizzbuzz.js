c0f44d0e1797059a9f74f8212c657b7f94df5d8a
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-03-05 07:24:31 -0300

    Finish the FizzBuzz backlog - first version

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,3 +1,9 @@
 module.exports = {
-  FizzBuzz:(value)=> (value % 3) === 0 ? 'Fizz': value
+  FizzBuzz: (value) => {
+    if(value%3 === 0 && value%5 === 0) return 'FizzBuzz'
+    if(value % 3 === 0) return 'Fizz' 
+    if(value % 5 === 0) return 'Buzz'
+    return value
+
+  }
 }
--- tests/fizzbuzz.spec.js
+++ tests/fizzbuzz.spec.js
@@ -16,5 +16,32 @@ import {FizzBuzz} from '../src/fizzbuzz.js'
 describe('Main',()=>{
   it('Should return Fizz when multiple of 3',()=>{
     expect(FizzBuzz(3)).toBe("Fizz")
+    expect(FizzBuzz(30)).not.toBe("Fizz")
+    expect(FizzBuzz(33)).toBe("Fizz")
+    expect(FizzBuzz(39)).toBe("Fizz")
+    expect(FizzBuzz(312)).toBe("Fizz")
+  })
+
+  it('Should return Buzz when multiple of 5',()=>{
+    expect(FizzBuzz(5)).toBe('Buzz')
+    expect(FizzBuzz(50)).toBe('Buzz')
+    expect(FizzBuzz(35)).toBe('Buzz')
+    expect(FizzBuzz(25)).toBe('Buzz')
+    expect(FizzBuzz(95)).toBe('Buzz')
+    expect(FizzBuzz(505)).toBe('Buzz')
+  })
+
+  it("Should return FizzBuzz when multiple of 3 and 5 at same time",()=>{
+    expect(FizzBuzz(15)).toBe('FizzBuzz')
+    expect(FizzBuzz(90)).toBe('FizzBuzz')
+    expect(FizzBuzz(45)).toBe('FizzBuzz')
+    expect(FizzBuzz(105)).toBe('FizzBuzz')
+  })
+
+  it("Should return value when not multiple of 3 and 5",()=>{
+    expect(FizzBuzz(7)).toBe(7)
+    expect(FizzBuzz(17)).toBe(17)
+    expect(FizzBuzz(16)).toBe(16)
+    expect(FizzBuzz(71)).toBe(71)
   })
 })
