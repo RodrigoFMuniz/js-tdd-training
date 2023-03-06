86530d9cbaa71f66e3a8020d74f74e614e13eeff
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-03-06 13:50:47 -0300

    coverage tests

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,6 +1,7 @@
 module.exports = {
   FizzBuzz: (value) => {
     if(typeof value !== 'number'){throw new Error('errrou')}
+    // if(typeof value === 'number'){return 0}
     if(value%3 === 0 && value%5 === 0) return 'FizzBuzz'
     if(value % 3 === 0) return 'Fizz' 
     if(value % 5 === 0) return 'Buzz'
